19fb94b7ffc4106f1b234360b5584c1ccd62703f
erebrus <erebrus@gmail.com>
2025-04-05 21:25:29 +0100

    noise and out of energy mechanics

--- godot/src/player/player.gd
+++ godot/src/player/player.gd
@@ -4,16 +4,21 @@ class_name Player extends RigidBody2D
 @export var base_thrust_timeout := .8
 @export var thrust := 400.0
 @export var back_thrust_factor := .2
-@export var strafe_thrust_factor := .3
+@export var strafe_thrust_factor := .25
 @export var full_thrust_bonus := 1.2
 @export var thrust_charge_speed :=2.0
+@export var min_thrust_timeout := .4
 @export_category("other movement")
 @export var rotation_speed :=2.5
 @export var friction := .7
 @export_category("energy")
 @export var max_energy:=100.0
-@export var energy_per_thrust:=1.0
+@export var energy_per_thrust:=10.0
 @export var no_energy_factor:=.5
+
+@export_category("noise")
+@export var noise_range:float = 600.0
+@export var thrust_noise_curve:Curve
 
 var can_thrust:=true
 var thrust_factor := 0.0
@@ -67,27 +72,32 @@ func charge_thrust(delta:float):
 		do_thrust()
 	
 func do_thrust(rotation_delta:float = 0):
-	Logger.trace("thrust %d" % Time.get_ticks_msec())
+	Logger.debug("thrust %d" % Time.get_ticks_msec())
 	if not has_energy():
 		thrust_factor *= no_energy_factor
 	var intensity:float = thrust * thrust_factor * (full_thrust_bonus if thrust_factor>=1.0 else 1)
-	Logger.info("thrust intensity %.2f" % intensity)
+	Logger.debug("thrust intensity %.2f" % intensity)
 	apply_impulse(Vector2.RIGHT.rotated(rotation+rotation_delta)*intensity,Vector2.ZERO)
+	do_noise()
 	can_thrust=false
 	$%ThrustState.color=Color("red")
 	animation_player.play("thrust")
+	$ThrustTimer.wait_time = max(min_thrust_timeout, base_thrust_timeout*thrust_factor *( 1 if has_energy() else 1.15))
+	Logger.debug("wait time %.2f" % $ThrustTimer.wait_time )
 	energy = max(0, energy-energy_per_thrust*thrust_factor)
-	$ThrustTimer.wait_time = base_thrust_timeout*thrust_factor
+
 	thrust_factor=0
 	$ThrustTimer.start()
-	Logger.trace("thrust NOT available %d" % Time.get_ticks_msec())
+	Logger.debug("thrust NOT available %d" % Time.get_ticks_msec())
 	Logger.info("light %.2f/%.2f" % [energy, light.energy])
 
-
+func do_noise():
+	Events.player_noise_ping.emit(global_position, noise_range*thrust_factor)
+	
 func _on_thrust_timer_timeout() -> void:
 	can_thrust=true
 	$%ThrustState.color=Color("white")
-	Logger.trace("thrust available %d" % Time.get_ticks_msec())
+	Logger.debug("thrust available %d" % Time.get_ticks_msec())
 	if not Input.is_action_pressed("move_forward"):
 		animation_player.play("thrust_to_idle")
 		await animation_player.animation_finished
--- godot/src/world/world.gd
+++ godot/src/world/world.gd
@@ -2,3 +2,4 @@ extends Node2D
 
 func _ready() -> void:
 	randomize()
+	Events.player_noise_ping.connect(func(pos, r): Logger.debug("Player noise ping at %s range: %.2f" % [pos,r]))
