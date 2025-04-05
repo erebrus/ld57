5faa3183cdf7cc5874a90c5a6ecea2a179cbf4ca
erebrus <erebrus@gmail.com>
2025-04-05 17:05:45 +0100

    play anim draft

--- godot/src/player/player.gd
+++ godot/src/player/player.gd
@@ -11,9 +11,12 @@ class_name Player extends RigidBody2D
 
 var can_thrust:=true
 var thrust_factor := 0.0
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
-
+func _ready():
+	animation_player.play("idle")
+	
 func _physics_process(delta: float) -> void:
 	var rotate_input:float = Input.get_axis("rotate_left","rotate_right")
 	if rotate_input:
@@ -38,6 +41,7 @@ func _physics_process(delta: float) -> void:
 func charge_thrust(delta:float):
 	if thrust_factor == 0:
 		thrust_factor = .2	
+		animation_player.play("charge")
 	else:
 		thrust_factor+=delta*thrust_charge_speed
 	if thrust_factor >=1.0:
@@ -48,6 +52,7 @@ func do_thrust(rotation_delta:float = 0):
 	apply_impulse(Vector2.RIGHT.rotated(rotation+rotation_delta)*thrust*thrust_factor,Vector2.ZERO)
 	can_thrust=false
 	$%ThrustState.color=Color("red")
+	animation_player.play("thrust")
 
 	$ThrustTimer.wait_time = base_thrust_timeout*thrust_factor
 	thrust_factor=0
@@ -56,4 +61,7 @@ func do_thrust(rotation_delta:float = 0):
 
 func _on_thrust_timer_timeout() -> void:
 	can_thrust=true
+	if not Input.is_action_pressed("move_forward"):
+		animation_player.play("idle")
+
 	$%ThrustState.color=Color("white")
