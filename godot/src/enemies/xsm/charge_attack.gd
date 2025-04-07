5bf9b80cc55a0ed40fb5a2ed7e4a0cb33d7b6b85
erebrus <erebrus@gmail.com>
2025-04-07 23:37:05 +0100

    Update charge_attack.gd

--- godot/src/enemies/xsm/charge_attack.gd
+++ godot/src/enemies/xsm/charge_attack.gd
@@ -6,13 +6,17 @@ var agent:Enemy
 func _on_enter(_args) -> void:
 	agent = target
 	Logger.info("%s %s" % [agent.name, self.name])
-	$"../../AnimationPlayer".play("move")
-	agent.nav_enabled = false
-	var delta_vector = (agent.target.global_position-agent.global_position)*2
-	var attack_dist = clamp(delta_vector.length(), 120,900)
-	agent.target_position = agent.global_position + delta_vector.normalized() * attack_dist
-	agent.current_speed=agent.max_speed
-	agent.player_on_target.connect(_on_player_on_target)
+	var agent_target = agent.target
+	if agent_target:
+		$"../../AnimationPlayer".play("move")
+		agent.nav_enabled = false
+		var delta_vector = (agent_target.global_position-agent.global_position)*2
+		var attack_dist = clamp(delta_vector.length(), 120,900)
+		agent.target_position = agent.global_position + delta_vector.normalized() * attack_dist
+		agent.current_speed=agent.max_speed
+		agent.player_on_target.connect(_on_player_on_target)
+	else:
+		change_state("Retreat")
 
 func _before_exit(_args) -> void:
 	agent.player_on_target.disconnect(_on_player_on_target)
