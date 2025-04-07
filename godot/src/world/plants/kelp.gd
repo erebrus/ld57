e27a2a02761ef08b58ef33ffcfe3f09bb6a24b1c
Ángela <angela.lbsr@gmail.com>
2025-04-07 15:56:02 +0200

    randomize kelp starting frame

--- godot/src/world/plants/kelp.gd
+++ godot/src/world/plants/kelp.gd
@@ -4,6 +4,8 @@ extends Area2D
 
 func _ready():
 	sprite.play("default")
+	sprite.frame = randi() % sprite.sprite_frames.get_frame_count("default")
+	
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
 		body.on_ruffle()
